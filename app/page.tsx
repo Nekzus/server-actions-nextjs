0e1e45c5b137b04289dba5d000f9618035549f80
Nekzus <85507589+Nekzus@users.noreply.github.com>
2023-09-03 22:58:20 -0300

    Change ubication page todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import FormTodo from "@/app/todo/components/form.todo";
+import ListTodo from "@/app/todo/components/list.todo";
+import { prisma } from "@/libs/prismadb";
 
-const HomePage = () => {
-  return <div>Homepage</div>;
+const TodoPage = async () => {
+  const todos = await prisma.todo.findMany();
+
+  return (
+    <div className="space-y-5">
+      <h1 className="text-center text-3xl my-10">Todos</h1>
+      <FormTodo />
+      <ListTodo todos={todos} />
+      {/* <pre>{JSON.stringify(todos, null, 2)}</pre> */}
+    </div>
+  );
 };
 
-export default HomePage;
+export default TodoPage;
